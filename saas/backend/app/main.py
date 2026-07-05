27e710988cc3e76eccb9bf62cb3101a51b996d61
sivaadityacoder <sivaaditya456@gmail.com>
2026-07-05 11:20:04 +0530

    Fix CORS hardcoded

--- saas/backend/app/main.py
+++ saas/backend/app/main.py
@@ -76,7 +76,12 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
+    allow_origins=[
+        "http://localhost:3000",
+        "https://app.adityasecuritylabs.tech",
+        "https://frontend-hfi1dy6j2-sivaadityacoders-projects.vercel.app",
+        "https://frontend-rmvqp5brd-sivaadityacoders-projects.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
